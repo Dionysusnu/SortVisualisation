3e7d09e00a50043bbefcf7577a68840fe3d039c6
Dionysusnu <dion.niessen@gmail.com>
2019-10-01 15:34:36 +0200

    Fix bug in delay

--- sorts/MergeSort.js
+++ sorts/MergeSort.js
@@ -1,7 +1,6 @@
 /* global sleep */
 async function MergeSort(array, comparison, starti, numElements) {
 	if (numElements > 1) {
-		await sleep(1);
 		const half = Math.floor(numElements / 2);
 		await MergeSort(array, comparison, starti, half);
 		await MergeSort(array, comparison, starti + half, numElements - half);
@@ -9,6 +8,7 @@ async function MergeSort(array, comparison, starti, numElements) {
 		const Rarray = array.slice(starti + half, starti + numElements);
 		let insertPos = starti;
 		while (Larray.length && Rarray.length) {
+			await sleep(1);
 			const Lelement = Larray.shift();
 			const Relement = Rarray.shift();
 			if (comparison(Lelement, Relement)) {
