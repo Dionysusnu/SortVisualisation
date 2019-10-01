e8629ae1f2152a62602dcf00c7596629fe3b4f3d
Dionysusnu <dion.niessen@gmail.com>
2019-10-01 14:44:53 +0200

    Add InsertionSort

--- sorts/InsertionSort.js
+++ sorts/InsertionSort.js
@@ -1,7 +1,21 @@
 /* global sleep */
 async function InsertionSort(array, comparison) {
-	// WIP
-	const sortedi = 1;
+	for (let i = 1; i < array.length; i++) {
+		await sleep(1);
+		const element = array[i];
+		let sorted = false;
+		let comparing = i;
+		while (!sorted) {
+			comparing--;
+			if (comparing < 0) {
+				sorted = true;
+			} else if (!comparison(array[comparing], element)) {
+				sorted = true;
+			} else {
+				array.swap(comparing, comparing + 1);
+			}
+		}
+	}
 }
 
 export { InsertionSort };
